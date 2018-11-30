a33e89be7c68bdf7909e4ac3149aedef9ea5c45c
Mahmoud Hashemi <mahmoud@hatnote.com>
2018-11-29 16:25:16 -0800

    more config loading, article list now loading

--- pacetrack/update.py
+++ pacetrack/update.py
@@ -6,6 +6,8 @@
   Update script to load data for tracked wikiproject campaigns
 
 """
+from __future__ import print_function
+
 import os
 import sys
 import json
@@ -15,6 +17,8 @@ from time import strftime
 from pipes import quote as shell_quote
 from argparse import ArgumentParser
 
+import attr
+from ruamel import yaml
 from boltons.fileutils import atomic_save
 
 from log import tlog, set_debug
@@ -35,10 +39,39 @@ def to_unicode(obj):
         return unicode(obj, encoding='utf8')
 
 
-
+@attr.s
 class PaceTracker(object):
-    pass
+    name = attr.ib()
+    lang = attr.ib()
+    requested_by = attr.ib()
+    wikiproject_name = attr.ib()
+    campaign_start_date = attr.ib()
+    campaign_end_date = attr.ib()
+    date_created = attr.ib()
+    article_list = attr.ib(repr=False)
 
+    @classmethod
+    def from_path(cls, path):
+        print(path)
+        config_data = yaml.safe_load(open(path + '/config.yaml', 'rb'))
+
+        # TODO: add sparql query and wikiproject support
+        article_list_filename = config_data.pop('article_list', "article_list.yaml")
+        article_list_path = path + '/' + article_list_filename
+        article_list = yaml.safe_load(open(article_list_path, 'rb'))
+        kwargs = dict(config_data)
+        kwargs['article_list'] = article_list['articles'] or []
+        return cls(**kwargs)
+
+    @classmethod
+    def create_from_config(cls, name):
+        """
+        make a directory in campaign path
+        make a data in that
+        initialize the config
+        etc.
+        """
+        pass
 
 
 def get_argparser():
@@ -58,12 +91,13 @@ def process_one(campaign_dir):
     # fetch data
     # output timestamped json file to campaign_dir/data/_timestamp_.json
     # generate static pages
-    print campaign_dir
+    pt = PaceTracker.from_path(campaign_dir)
+    print(pt)
 
 
 def process_all():
     for campaign_dir in os.listdir(CAMPAIGNS_PATH):
-        process_one(campaign_dir)
+        process_one(CAMPAIGNS_PATH + campaign_dir)
 
 
 @tlog.wrap('critical')
@@ -76,9 +110,9 @@ def main():
         if args.debug:
             set_debug(True)
         process_all()
-        print 'success'
+        print('success')
     except Exception:
-        pass
+        raise  # TODO
 
 
 
