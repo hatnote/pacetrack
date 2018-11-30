baa1e77374b97b20611978e52c28c7b8b48c4589
Stephen LaPorte <stephen.laporte@gmail.com>
2018-11-30 00:00:36 -0800

    added a few more simple metrics

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pacetrack
 Progress tracker for WikiProject improvement campaigns, started at WikiCite 2018
+
+## Features
+Track the following features in your Wikiproject:
+
+ - [x] Exists on Wikipedia
+ - [x] Exists on Wikidata
+ - [x] Has infobox
+ - [x] Infobox is Wikidata-powered
+ - [x] Number of citations
+ - [x] Percentage of citations are Wikidata-powered
+ - [ ] Which maintence templates
+ - [ ] How many categories
+ - [x] Is in Wikiproject:Newspapers
+ - [x] Quality assessment (only at runtime)
+ - [x] Importance assessment (only at runtime)
+ - [ ] How many characters
+ - [ ] How many sections
+ - [ ] How many interlanguage links
--- pacetrack/metrics.py
+++ pacetrack/metrics.py
@@ -2,18 +2,25 @@ import re
 from collections import namedtuple
 import requests
 
-def getPageRevIdAtTimestamp(titles, timestamp):
+
+MW_API_URL = 'https://en.wikipedia.org/w/api.php'
+REST_API_URL = 'https://en.wikipedia.org/api/rest_v1/page/references/%s/%s'
+
+
+def getPageRevIdAtTimestamp(title, timestamp):
     """Get page revision id at a particular timestamp
 
-    :param titles: a list of page titles
+    :param title: a page title; note, the MW API only supports a
+    single title in titles when using rvstart
     :param timestamp: in the form of ISO8601 '2018-11-29T20:09:07Z'
     :return: a map from page title to the revision id
+
     """
     resp = requests.get('https://en.wikipedia.org/w/api.php', params={
         'action': 'query',
         'prop': 'revisions',
         'format': 'json',
-        'titles': '|'.join(titles),
+        'titles': title,
         'rvlimit': '1',
         'rvstart': timestamp
     }).json()
@@ -49,3 +56,92 @@ def getCiteQStats(oldid):
     nonCiteQCounts = sum([entry.calls for entry in templatecalls if entry.template in noCiteQTemplates])
     citeQCounts = sum([entry.calls for entry in templatecalls if entry.template in citeQTemplates])
     return (citeQCounts, nonCiteQCounts)
+
+
+def get_article_wikidata_item(oldid):
+    params = {'action': 'query',
+              'prop': 'wbentityusage',
+              'revids': oldid,
+              'format': 'json'}
+    resp = requests.get(MW_API_URL, params)
+    wbentities = resp.json()['query']['pages'].values()[0]['wbentityusage']
+    # 'T' aspect means the Wikidata item corresponds to the title of the page
+    # I'm assuming there is only corresponding Wikidata item; maybe that's
+    # not safe?
+    return [q for (q, val) in wbentities.items() if 'T' in val['aspects']][0]
+
+
+def get_pageassessments(title):
+    # I don't think you can actually get assessments from past
+    # versions of an article
+    params = {'action': 'query',
+              'prop': 'pageassessments',
+              'titles': title,
+              'formatversion': 2,
+              'format': 'json'}
+    resp = requests.get(MW_API_URL, params)
+    return resp.json()['query']['pages'][0]['pageassessments']
+    
+
+
+def check_existence(title, timestamp):
+    try:
+        oldid_map = getPageRevIdAtTimestamp(title, timestamp)
+    except KeyError as e:
+        return False
+    return True
+
+
+def check_infobox(template_calls):
+    for template_call in template_calls:
+        if 'infobox' in template_call.template.lower():
+            return True
+    return False
+
+
+def check_infobox_wikidata(template_calls):
+    wikidata_template_pattern = re.compile(r'infobox(.*)\/wikidata')
+    for template_call in template_calls:
+        if re.search(wikidata_template_pattern, template_call.template.lower()):
+            return True
+    return False
+
+
+def check_wikiproject(wikiproject, assessments):
+    if wikiproject in assessments.keys():
+        return True
+    return False
+
+
+def get_citation_stats(title, oldid):
+    article_cites_rest_url = REST_API_URL % (title, oldid)
+    
+    resp = requests.get(article_cites_rest_url)
+    citations = resp.json()
+    
+    ref_count = len(citations['references_by_id'].keys())
+    ref_wikidata_count = len([c for c in citations['references_by_id'].items()
+                              if 'https://www.wikidata.org/wiki/Q'
+                              in c[1]['content']['html']])
+    ref_wikidata_percent = ref_wikidata_count / (ref_count * 1.0)
+    
+    return {'reference_count': ref_count,
+            'reference_wikidata_count': ref_wikidata_count,
+            'reference_wikidata_percent': ref_wikidata_percent}
+
+if __name__ == '__main__':
+    test_article = 'The Register-Guard'
+
+    existence_2005 = check_existence(test_article, '2005-01-01T00:00:00Z')
+    existence_2018 = check_existence(test_article, '2018-11-29T20:00:00Z')
+
+    oldid_map = getPageRevIdAtTimestamp(test_article, '2018-11-30T00:00:00Z')
+    oldid = oldid_map[test_article]    
+
+    citation_stats = get_citation_stats(test_article, oldid)
+    templates = getTemplatesForRevision(oldid)
+    wd_item = get_article_wikidata_item(oldid)
+    assessments = get_pageassessments(test_article)
+
+    import pdb;pdb.set_trace()
+
