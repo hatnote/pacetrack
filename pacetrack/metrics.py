88d2ccf596a4f10369d90ea5ac10171575603749
Stephen LaPorte <stephen.laporte@gmail.com>
2018-12-08 12:51:16 -0800

    added function to get stats per article

--- pacetrack/metrics.py
+++ pacetrack/metrics.py
@@ -1,13 +1,13 @@
 import re
-from collections import namedtuple
 import requests
+from collections import namedtuple
 
 
 MW_API_URL = 'https://en.wikipedia.org/w/api.php'
 REST_API_URL = 'https://en.wikipedia.org/api/rest_v1/page/references/%s/%s'
 
 
-def getPageRevIdAtTimestamp(title, timestamp):
+def get_revid_at_timestamp(title, timestamp):
     """Get page revision id at a particular timestamp
 
     :param title: a page title; note, the MW API only supports a
@@ -24,10 +24,14 @@ def getPageRevIdAtTimestamp(title, timestamp):
         'rvlimit': '1',
         'rvstart': timestamp
     }).json()
-    return {page['title']: page['revisions'][0]['revid'] for page in resp['query']['pages'].values()}
+    try:
+        ret = {page['title']: page['revisions'][0]['revid'] for page in resp['query']['pages'].values()}
+    except KeyError as e:
+        return {title: None}
+    return ret
 
 
-def getTemplatesForRevision(oldid):
+def get_templates(oldid):
     """Get a list of templates as well as number of calls per template for a given revision (oldid)
 
     NOTE: we parse the info from the transclusion expansion report from the parse API, which might be unstable.
@@ -39,23 +43,10 @@ def getTemplatesForRevision(oldid):
         'oldid': oldid,
         'format': 'json',
     }).json()
-    revisionText = revisionResponse['parse']['text']['*']
-    transclusionReport = revisionText[revisionText.find('<!--\nTransclusion expansion time report'):]
-    TemplateCalls = namedtuple('TemplateCalls', ('template', 'calls'))
-    templatePattern = re.compile(' (\d+) Template:(.*)\n', re.MULTILINE)
-    matches = re.finditer(templatePattern, transclusionReport)
 
-    return [TemplateCalls(match.group(2), int(match.group(1))) for match in matches]
-
-
-def getCiteQStats(oldid):
-    """Get citation stats that is using CiteQ vs non-CiteQ"""
-    templatecalls = getTemplatesForRevision(oldid)
-    noCiteQTemplates = {'Cite_web', 'Cite_web'}
-    citeQTemplates = {'Cite_Q', 'Cite_q'}
-    nonCiteQCounts = sum([entry.calls for entry in templatecalls if entry.template in noCiteQTemplates])
-    citeQCounts = sum([entry.calls for entry in templatecalls if entry.template in citeQTemplates])
-    return (citeQCounts, nonCiteQCounts)
+    templates = revisionResponse['parse']['templates']
+    ret = [t['*'].replace('Template:', '') for t in templates]
+    return ret
 
 
 def get_article_wikidata_item(oldid):
@@ -64,14 +55,18 @@ def get_article_wikidata_item(oldid):
               'revids': oldid,
               'format': 'json'}
     resp = requests.get(MW_API_URL, params)
-    wbentities = resp.json()['query']['pages'].values()[0]['wbentityusage']
+    try:
+        wbentities = resp.json()['query']['pages'].values()[0]['wbentityusage']
+    except KeyError as e:
+        return None
+    
     # 'T' aspect means the Wikidata item corresponds to the title of the page
     # I'm assuming there is only corresponding Wikidata item; maybe that's
     # not safe?
     return [q for (q, val) in wbentities.items() if 'T' in val['aspects']][0]
 
 
-def get_pageassessments(title):
+def get_assessments(title):
     # I don't think you can actually get assessments from past
     # versions of an article
     params = {'action': 'query',
@@ -94,7 +89,7 @@ def check_existence(title, timestamp):
 
 def check_infobox(template_calls):
     for template_call in template_calls:
-        if 'infobox' in template_call.template.lower():
+        if 'infobox' in template_call.lower():
             return True
     return False
 
@@ -102,13 +97,17 @@ def check_infobox(template_calls):
 def check_infobox_wikidata(template_calls):
     wikidata_template_pattern = re.compile(r'infobox(.*)\/wikidata')
     for template_call in template_calls:
-        if re.search(wikidata_template_pattern, template_call.template.lower()):
+        if re.search(wikidata_template_pattern, template_call.lower()):
             return True
     return False
 
 
-def check_wikiproject(wikiproject, assessments):
-    if wikiproject in assessments.keys():
+def get_wikiproject(wikiproject, talk_revid):
+    talk_templates = get_templates(talk_revid)
+    wikiprojects = [tc.replace('WikiProject ', '')  for tc in talk_templates
+                    if 'wikiproject' in tc.lower()]
+
+    if wikiproject in wikiprojects:
         return True
     return False
 
@@ -118,30 +117,62 @@ def get_citation_stats(title, oldid):
     
     resp = requests.get(article_cites_rest_url)
     citations = resp.json()
-    
-    ref_count = len(citations['references_by_id'].keys())
+
+    try:
+        ref_count = len(citations['references_by_id'].keys())
+    except KeyError as e:
+        return {'reference_count': 0,
+                'reference_wikidata_count': 0,
+                'reference_wikidata_percent': 0}
+
     ref_wikidata_count = len([c for c in citations['references_by_id'].items()
                               if 'https://www.wikidata.org/wiki/Q'
                               in c[1]['content']['html']])
-    ref_wikidata_percent = ref_wikidata_count / (ref_count * 1.0)
-    
+    if ref_count:
+        ref_wikidata_percent = ref_wikidata_count / (ref_count * 1.0)
+    else:
+        ref_wikidata_percent = 0
+        
     return {'reference_count': ref_count,
             'reference_wikidata_count': ref_wikidata_count,
             'reference_wikidata_percent': ref_wikidata_percent}
 
+def get_all_stats(title, wikiproject, date):
+    revid_map = get_revid_at_timestamp(title, date)
+    revid = revid_map[title]
+
+    talk_revid_map = get_revid_at_timestamp('Talk:' + title, date)
+    talk_revid = talk_revid_map['Talk:' + title]
+    
+    templates = get_templates(revid)
+    assessments = get_assessments(title)
+
+    stats = {'wikipedia_exists': revid,
+             'wikidata_exists': get_article_wikidata_item(revid),
+             'infobox': check_infobox(templates),
+             'infobox_wikidata': check_infobox_wikidata(templates),
+             'citation_stats': get_citation_stats(title, revid),
+             'in_wikiproject': get_wikiproject(wikiproject, talk_revid),
+             'quality': assessments.get(wikiproject, {}).get('class'),
+             'importance': assessments.get(wikiproject, {}).get('importance'),
+             'metadata': {'target_date': date,
+                          'revision': revid,
+                          'talk_revision': talk_revid}}
+    
+    return stats
+
+
 if __name__ == '__main__':
-    test_article = 'The Register-Guard'
+    title = 'Coffee'
+    wikiproject = 'Newspapers'
+    date = '2018-12-05T20:00:00Z'
 
-    existence_2005 = check_existence(test_article, '2005-01-01T00:00:00Z')
-    existence_2018 = check_existence(test_article, '2018-11-29T20:00:00Z')
+    stats = get_all_stats(title, wikiproject, date)
+    print stats
 
-    oldid_map = getPageRevIdAtTimestamp(test_article, '2018-11-30T00:00:00Z')
-    oldid = oldid_map[test_article]    
-
-    citation_stats = get_citation_stats(test_article, oldid)
-    templates = getTemplatesForRevision(oldid)
-    wd_item = get_article_wikidata_item(oldid)
-    assessments = get_pageassessments(test_article)
-
+    old_date = '2010-12-05T20:00:00Z'
+    stats = get_all_stats(title, wikiproject, old_date)
+    print stats
+    
     import pdb;pdb.set_trace()
 
